5f61c3231a68ed97db855263e8f156fdd94ede04
wagnergithub <maurice.wagner@yandex.com>
2017-03-04 20:39:57 +0100

    Add files via upload

---
+++ Python/1.py
@@ -0,0 +1,8 @@
+z = int(input("Type in a number"));
+x = int(input("Type in a number"));
+
+def addNum(z,x):
+    f = z +x;
+    return f;
+
+print (addNum(z,x));
---
+++ Python/2.py
@@ -0,0 +1,17 @@
+print("Celsius---Fahrenheit");
+for x in range(-15,36):
+    tempf = x *9
+    tempf = tempf /5;
+    tempf = tempf +32;
+    print ("%.2f"%x,"---","%.2f"%tempf);
+c = float(input("Type in a number to convert"));
+c = (c *9)/5;
+c = c +32;
+print (c);
+    
+
+
+ 
+
+
+
